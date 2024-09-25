e778449d9a680fa7f5f8d469d8498172c1b4cf49
Lindah-K <lindahkima@gmail.com>
2024-09-25 20:55:05 +0300

    update

--- orders/urls.py
+++ orders/urls.py
@@ -1,6 +1,7 @@
 from django.urls import path, include
 from rest_framework.routers import DefaultRouter
-from .views import CustomerViewSet, OrderViewSet, login
+from .views import CustomerViewSet, OrderViewSet
+from mozilla_django_oidc.views import OIDCAuthenticationResponseView
 
 router = DefaultRouter()
 router.register(r'customers', CustomerViewSet, basename='customer')
@@ -8,7 +9,7 @@ router.register(r'orders', OrderViewSet, basename='order')
 
 urlpatterns = [
     path('', include(router.urls)),
-    path('login/', login, name='login'),
+    path('login/', OIDCAuthenticationResponseView.as_view(), name='oidc_login')
 ]
 
 
